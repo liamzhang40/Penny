c862ede5f78d18b7a79ac8cc19069cd11df8cb51
Liangtian Zhang <lzhang40@binghamton.edu>
2018-11-06 16:28:37 -0500

    added superuser creation

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,28 +84,28 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #     }
 # }
 
-# if 'RDS_HOSTNAME' in os.environ:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'NAME': os.environ['RDS_DB_NAME'],
-#             'USER': os.environ['RDS_USERNAME'],
-#             'PASSWORD': os.environ['RDS_PASSWORD'],
-#             'HOST': os.environ['RDS_HOSTNAME'],
-#             'PORT': os.environ['RDS_PORT'],
-#         }
-#     }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'penny',
-        'USER': 'root',
-        'PASSWORD': 'db123456',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'penny',
+            'USER': 'root',
+            'PASSWORD': 'db123456',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+        }
+    }
 
 
 # Password validation
