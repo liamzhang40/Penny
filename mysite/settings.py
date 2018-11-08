bbb31bd4d23079a865332158447a11dcdb203714
Liangtian Zhang <lzhang40@binghamton.edu>
2018-11-07 19:26:17 -0500

    re-configured the staticfile path, hopefully it works this time

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,9 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "assets", "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR),
+    "",
 )
 # webpack 
 
