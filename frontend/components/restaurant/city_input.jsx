10ce47a393f3b4bdb11a7361372fed81f96f4e9a
Liangtian Zhang <lzhang40@binghamton.edu>
2018-11-10 13:58:24 -0500

    added styling to city search button

--- frontend/components/restaurant/city_input.jsx
+++ frontend/components/restaurant/city_input.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import { fetchRestaurants } from '../../actions/restaurant_actions';
 
 const mapDispatchToProps = dispatch => {
@@ -7,6 +8,10 @@ const mapDispatchToProps = dispatch => {
         fetchRestaurants: (city, offset) => dispatch(fetchRestaurants(city, offset))
     };
 };
+
+const Button = styled.button`
+    background-color: #bd1f1f;
+`;
 
 class CityInput extends React.Component {
     constructor() {
@@ -40,11 +45,11 @@ class CityInput extends React.Component {
                         placeholder="city name"
                         onChange={this.update("city")} />
                 </label>
-                <button className="search">
+                <Button>
                     <svg viewBox="0 0 24 24" width="20px">
                         <path d="M20.753 19.34l-4.295-4.297A7.46 7.46 0 0 0 18 10.5a7.5 7.5 0 1 0-7.5 7.5 7.46 7.46 0 0 0 4.543-1.542l4.296 4.295a1 1 0 1 0 1.412-1.414zM10.5 16A5.506 5.506 0 0 1 5 10.5C5 7.467 7.467 5 10.5 5S16 7.467 16 10.5 13.533 16 10.5 16z"/>
                     </svg>
-                </button>
+                </Button>
             </form>
         );
     }
