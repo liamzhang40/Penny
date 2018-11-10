b9a5238c726b2815e5c2c9f466448ed8dc315ef7
Jonathan Ng <jng20@binghamton.edu>
2018-11-10 00:03:06 -0500

    modified forms.py

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,9 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age',) 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age',)
