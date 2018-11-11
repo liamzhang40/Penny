5cc6019248e9f9ab2776ae12a3d3f4cdb71b841d
Liangtian Zhang <lzhang40@binghamton.edu>
2018-11-10 22:24:25 -0500

    finished styling top nav bar for now

--- frontend/components/restaurant/city_input.jsx
+++ frontend/components/restaurant/city_input.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
-import { Button } from '../styles';
+import { Button, Label } from '../styles';
 import { fetchRestaurants } from '../../actions/restaurant_actions';
+
+const Span = styled.span`
+    margin-right: 10px;
+    font-weight: 700;
+`;
 
 const SearchButton = styled(Button)`
     padding: 6px 12px;
@@ -56,14 +61,14 @@ class CityInput extends React.Component {
         return (
             <RestaurantTopNavBar>
                 <RestaurantTopNavBarForm onSubmit={this.handleSubmit}>
-                    <label className="city-search-label">
-                        Find Restaurants Near
+                    <Label className="city-search-label">
+                        <Span>Find Restaurants Near</Span>
                         <input
                             className="city-search"
                             type="text"
                             placeholder="city name"
                             onChange={this.update("city")} />
-                    </label>
+                    </Label>
                     <SearchButton>
                         <svg viewBox="0 0 24 24" width="20px">
                             <path d="M20.753 19.34l-4.295-4.297A7.46 7.46 0 0 0 18 10.5a7.5 7.5 0 1 0-7.5 7.5 7.46 7.46 0 0 0 4.543-1.542l4.296 4.295a1 1 0 1 0 1.412-1.414zM10.5 16A5.506 5.506 0 0 1 5 10.5C5 7.467 7.467 5 10.5 5S16 7.467 16 10.5 13.533 16 10.5 16z"/>
--- frontend/components/styles.jsx
+++ frontend/components/styles.jsx
@@ -3,8 +3,15 @@ import styled, { css } from 'styled-components';
 export const Button = styled.button`
     background-color: #bd1f1f;
     fill: #fff;
-    
+
     &:hover {
         background-color: #a71c1c;
     }
+`;
+
+export const Label = styled.label`
+    font-size: 14px;
+    background-color: #fff;
+    padding: 8px 12px;
+    border-radius: 4px 0 0 4px;
 `;
