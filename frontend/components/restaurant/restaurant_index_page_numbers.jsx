7d10874ff0bb9e100a43ccf24ba3acd1c5434d3d
Liangtian Zhang <lzhang40@binghamton.edu>
2018-11-22 13:57:13 -0500

    added page number styling

--- frontend/components/restaurant/restaurant_index_page_numbers.jsx
+++ frontend/components/restaurant/restaurant_index_page_numbers.jsx
@@ -9,24 +9,51 @@ const PageNumbers = styled.ul`
 
 const Page = styled.li`
     cursor: pointer;
+    color: #0073bb;
     padding: 12px 6px 9px;
     border-bottom: 3px solid transparent;
+    font-size: 13px;
+
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
+const SelectedPage = styled.li`
+    color: black;
+    padding: 12px 6px 9px;
+    border-bottom: 3px solid #d32323;
     font-size: 13px;
 `;
 
 class RestaurantIndexPageNumbers extends React.Component {
-    
-    handleClick = e => {
-        
+    constructor() {
+        super();
+
+        this.state = {
+            selectedPage: "1"
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        // this.setState((prevState, nextProps) => (
+        //     {SelectedPage: e.currentTarget.content}
+        // ))
+        // debugger
+        this.setState({selectedPage: e.target.innerText})
     }
 
     render() {
         const pages = new Array(9).fill(undefined).map((el, page) => (
+            this.state.selectedPage === String(page + 1) ?
+            <SelectedPage key={page}>{page + 1}</SelectedPage> :
             <Page key={page}>{page + 1}</Page>
         ))
     
         return (
-            <PageNumbers onClick={handleClick}>
+            <PageNumbers onClick={this.handleClick}>
                 {pages}
             </PageNumbers>
         );
