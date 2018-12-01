a78e55686297e755b137401d4c396c7338bfee86
Siu Ka <jwong4494@gmail.com>
2018-11-30 19:55:19 -0500

    re-made sign-up and log-in page

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -3,13 +3,17 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
-        fields = ('username', 'email', 'age',) 
+	class Meta(UserCreationForm.Meta):
+		model = CustomUser
+		fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'age',)
+		fields = ('first_name', 'last_name', 'username', 'email', 'age',)
+		labels = {
+			'first_name': 'First Name',
+			'last_name': 'Last Name',
+		}
 
 class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = CustomUser
-        fields = UserChangeForm.Meta.fields
-        fields = ('username', 'email', 'age',)
+	class Meta:
+		model = CustomUser
+		fields = UserChangeForm.Meta.fields
+		fields = ('username', 'email', 'age',)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django import forms
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
+	first_name = models.CharField(forms.TextInput(attrs={'class':'form-contro', 
+														 'placeholder': 'First Name'}),
+								  max_length=32,
+								  help_text="First name")
+	last_name = models.CharField(forms.TextInput(attrs={'class':'form-control', 
+														'placeholder': 'Last name'}),
+								 max_length=32,
+								 help_text="Last name")
+	age = models.PositiveIntegerField(null=True, blank=True)
